3463ccff28b1c0497d03722495f05a7e22950ac0
Imane OUADA <imaneouada2002@gmail.com>
2023-11-20 17:53:57 +0100

    Initial commit

---
+++ app/src/main/java/com/euromedcompany/orderfood/MainActivity.java
@@ -0,0 +1,44 @@
+package com.euromedcompany.orderfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    TextView txtSlogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        txtSlogan = findViewById(R.id.txtSlogan);
+
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/NABILA.TTF");
+        txtSlogan.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Add your onClick logic for btnSignIn
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Add your onClick logic for btnSignUp
+            }
+        });
+    }
+
+}
+
