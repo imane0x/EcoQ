a4a851ccd4b56cbb8767309f4e25a1004336ed93
Imane <imaneouada2002@gmail.com>
2023-11-22 22:36:43 +0100

    add card views xml

---
+++ app/src/main/java/com/euromedcompany/orderfood/MainActivityCard.java
@@ -0,0 +1,14 @@
+package com.euromedcompany.orderfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_card);
+    }
+}
