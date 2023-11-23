59c20ec38429c1af50cf5fb6928b50a9f9ad954b
ohidaoui <ou.hidaoui@gmail.com>
2023-11-23 21:57:17 +0100

    changed HomeFragment to activate cards

--- app/src/main/java/com/euromedcompany/orderfood/HomeFragment.java
+++ app/src/main/java/com/euromedcompany/orderfood/HomeFragment.java
@@ -1,19 +1,90 @@
 package com.euromedcompany.orderfood;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.euromedcompany.orderfood.databinding.FragmentHomeBinding;
+
 
 public class HomeFragment extends Fragment {
+
+    //setContentView(R.layout.activity_main_card);
+    FragmentHomeBinding binding;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(getLayoutInflater());
+
+        binding.videoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new VideoFragment());
+            }
+        });
+        binding.shortsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new ShortsFragment());
+            }
+        });
+        binding.liveCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new LiveFragment());
+            }
+        });
+        binding.exploreCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new ExploreFragment());
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+/*
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = FragmentHomeBinding.inflate(getLayoutInflater());
+        binding.videoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new VideoFragment());
+            }
+        });
+        binding.shortsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new ShortsFragment());
+            }
+        });
+        binding.liveCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new LiveFragment());
+            }
+        });
+        binding.exploreCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new ExploreFragment());
+            }
+        });
+    }
+ */
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getChildFragmentManager();//getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/euromedcompany/orderfood/MainActivityCard.java
+++ app/src/main/java/com/euromedcompany/orderfood/MainActivityCard.java
@@ -16,41 +16,14 @@ public class MainActivityCard extends AppCompatActivity {
         ActivityMainCardBinding binding;
 
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main_card);
-            videoCard = findViewById(R.id.videoCard);
-            shortsCard = findViewById(R.id.shortsCard);
-            liveCard = findViewById(R.id.liveCard);
-            exploreCard = findViewById(R.id.exploreCard);
-            videoCard.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivityCard.this, VideoCardActivity.class);
-                    startActivity(intent);
-                }
-            });
-            shortsCard.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivityCard.this, ShortsCardActivity.class);
-                    startActivity(intent);
-                }
-            });
-            liveCard.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivityCard.this, LiveCardActivity.class);
-                    startActivity(intent);
-                }
-            });
-            exploreCard.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivityCard.this, ExploreCardActivity.class);
-                    startActivity(intent);
-                }
-            });
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+            //setContentView(R.layout.activity_main_card);
+            //videoCard = findViewById(R.id.videoCard);
+            //shortsCard = findViewById(R.id.shortsCard);
+            //liveCard = findViewById(R.id.liveCard);
+            //exploreCard = findViewById(R.id.exploreCard);
+
 
         binding = ActivityMainCardBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -73,7 +46,7 @@ public class MainActivityCard extends AppCompatActivity {
         });
 
     }
-       private void replaceFragment(Fragment fragment) {
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
