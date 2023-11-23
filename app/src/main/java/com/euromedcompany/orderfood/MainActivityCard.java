01e0f3df47a8f248092ee38288343830488e21aa
ohidaoui <ou.hidaoui@gmail.com>
2023-11-23 23:13:19 +0100

    changed background and font in home fragment

--- app/src/main/java/com/euromedcompany/orderfood/MainActivityCard.java
+++ app/src/main/java/com/euromedcompany/orderfood/MainActivityCard.java
@@ -3,27 +3,18 @@ package com.euromedcompany.orderfood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import androidx.cardview.widget.CardView;
-import android.content.Intent;
-import android.view.View;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import com.euromedcompany.orderfood.databinding.ActivityMainCardBinding;
 
 public class MainActivityCard extends AppCompatActivity {
-        CardView videoCard, shortsCard, liveCard, exploreCard;
         ActivityMainCardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-            //setContentView(R.layout.activity_main_card);
-            //videoCard = findViewById(R.id.videoCard);
-            //shortsCard = findViewById(R.id.shortsCard);
-            //liveCard = findViewById(R.id.liveCard);
-            //exploreCard = findViewById(R.id.exploreCard);
-
 
         binding = ActivityMainCardBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
