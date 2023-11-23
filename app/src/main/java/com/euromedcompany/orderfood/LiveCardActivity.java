6d4d990684dfce97306a38c839d4273e12df9150
ohidaoui <ou.hidaoui@gmail.com>
2023-11-23 16:21:56 +0100

    add LiveCard activity and listners in MainCard activity

---
+++ app/src/main/java/com/euromedcompany/orderfood/LiveCardActivity.java
@@ -0,0 +1,14 @@
+package com.euromedcompany.orderfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LiveCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_card);
+    }
+}
--- app/src/main/java/com/euromedcompany/orderfood/MainActivityCard.java
+++ app/src/main/java/com/euromedcompany/orderfood/MainActivityCard.java
@@ -9,16 +9,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 public class MainActivityCard extends AppCompatActivity {
-        CardView videoCard;
+        CardView videoCard, shortsCard, liveCard, exploreCard;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main_card);
             videoCard = findViewById(R.id.videoCard);
+            shortsCard = findViewById(R.id.shortsCard);
+            liveCard = findViewById(R.id.liveCard);
+            exploreCard = findViewById(R.id.exploreCard);
             videoCard.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(MainActivityCard.this, VideoCardActivity.class);
+                    startActivity(intent);
+                }
+            });
+            shortsCard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivityCard.this, ShortsCardActivity.class);
+                    startActivity(intent);
+                }
+            });
+            liveCard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivityCard.this, LiveCardActivity.class);
+                    startActivity(intent);
+                }
+            });
+            exploreCard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivityCard.this, ExploreCardActivity.class);
                     startActivity(intent);
                 }
             });
