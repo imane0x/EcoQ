63a39467b50670004c5567c2df82f95fc32e0c03
Imane <imaneouada2002@gmail.com>
2023-12-14 18:46:40 +0100

    solve problem deleted description and title

--- app/src/main/java/com/euromedcompany/orderfood/ReportFragment.java
+++ app/src/main/java/com/euromedcompany/orderfood/ReportFragment.java
@@ -44,11 +44,11 @@ public class ReportFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_report, container, false);
 
-    //@Override
-    //protected void onCreate(Bundle savedInstanceState) {
+        //@Override
+        //protected void onCreate(Bundle savedInstanceState) {
         //   super.onCreate(savedInstanceState);
         // setContentView(R.layout.fragment_image);
-        uploadImage = view.findViewById(R.id.uploadImage);
+        uploadImage = view.findViewById(R.id.upload);
         issueDescription = view.findViewById(R.id.issueDescription);
         issueTypeSpinner = view.findViewById(R.id.issueTypeSpinner);
         issueTitle = view.findViewById(R.id.issueTitle);
@@ -81,13 +81,13 @@ public class ReportFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 saveData();
-                uploadImage = view.findViewById(R.id.uploadImage);
+               uploadImage = view.findViewById(R.id.upload);
                 //uploadImage.setImageURI(null); // Clear the image
-                uploadImage.setImageResource(R.drawable.upload);
-                issueTitle.setText("");
-                issueDescription.setText("");
-                issueTypeSpinner.setSelection(0);
-                uri = null;
+               // uploadImage.setImageResource(R.drawable.upload);
+//                issueTitle.setText("");
+//                issueDescription.setText("");
+//                issueTypeSpinner.setSelection(0);
+//                uri = null;
             }
         });
         return view;
@@ -144,153 +144,3 @@ public class ReportFragment extends Fragment {
                 });
     }
 }
-
-
-/*
-
-package com.euromedcompany.orderfood;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.text.DateFormat;
-import java.util.Calendar;
-
-public class ImageFragment extends Fragment {
-
-    private EditText uploadCaption;
-    private ImageView uploadImage;
-    private ProgressBar progressBar;
-    private FloatingActionButton uploadButton;
-
-    private String imageURL;
-    private Uri uri;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_image, container, false);
-
-        uploadCaption = view.findViewById(R.id.issueTitle);
-        uploadImage = view.findViewById(R.id.uploadImage);
-        progressBar = view.findViewById(R.id.progressBar);
-        uploadButton = view.findViewById(R.id.uploadButton);
-
-        progressBar.setVisibility(View.INVISIBLE);
-
-        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == getActivity().RESULT_OK){
-                            Intent data = result.getData();
-                            uri = data.getData();
-                            uploadImage.setImageURI(uri);
-                        } else {
-                            Toast.makeText(requireContext(), "No Image Selected", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }
-        );
-
-        uploadImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent photoPicker = new Intent(Intent.ACTION_PICK);
-                photoPicker.setType("image/*");
-                activityResultLauncher.launch(photoPicker);
-            }
-        });
-
-        uploadButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                uploadData();
-            }
-        });
-
-        return view;
-    }
-
-    private void uploadData() {
-        if (uri != null) {
-            StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Android Images")
-                    .child(uri.getLastPathSegment());
-
-            // Display a simple progress dialog
-            ProgressDialog progressDialog = new ProgressDialog(requireContext());
-            progressDialog.setMessage("Uploading...");
-            progressDialog.setCancelable(false);
-            progressDialog.show();
-
-            storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
-                    while (!uriTask.isComplete());
-                    Uri urlImage = uriTask.getResult();
-                    imageURL = urlImage.toString();
-                    uploadToDatabase();
-                    progressDialog.dismiss();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    progressDialog.dismiss();
-                    Toast.makeText(requireContext(), "Upload Failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            });
-        } else {
-            Toast.makeText(requireContext(), "No Image Selected", Toast.LENGTH_SHORT).show();
-        }
-    }
-    private void uploadToDatabase() {
-        String caption = uploadCaption.getText().toString();
-
-        DataClass dataClass = new DataClass(imageURL,caption);
-
-        String currentDate = DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
-
-        FirebaseDatabase.getInstance().getReference("Image Captions").child(currentDate)
-                .setValue(dataClass).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()){
-                            Toast.makeText(requireContext(), "Upload Successful", Toast.LENGTH_SHORT).show();
-                            // Note: If you are using this fragment within an activity, you might want to call getActivity().finish() instead
-                            requireActivity().finish();
-                        } else {
-                            Toast.makeText(requireContext(), "Upload Failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-}
-
-*/
